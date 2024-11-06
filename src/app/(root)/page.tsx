04fa8daf61b0600e0a7106c6617e95d0caf952ef
Bernard065 <bebenibernard@gmail.com>
2024-11-06 16:38:11 +0300

    Implement Mobile Navigation

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,24 +1,9 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
 import React from "react";
 
 const Home = async () => {
-  const session = await auth;
-
-  console.log("ddff", session);
-
   return (
     <>
       <h1>Home</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      ><Button type="submit">Log out</Button></form>
     </>
   );
 };
