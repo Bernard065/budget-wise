89430c4748f695c215d70ef41ba13c000efb26de
Bernard065 <bebenibernard@gmail.com>
2024-11-23 23:52:13 +0300

    Implement the upload functionality for the csv file

---
+++ src/app/(root)/transactions/upload-button.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useCSVReader } from "react-papaparse";
+
+import { Button } from "@/components/ui/button";
+import { Upload } from "lucide-react";
+
+type Props = {
+  onUpload: (results) => void;
+};
+
+const UploadButton = ({ onUpload }: Props) => {
+  const { CSVReader } = useCSVReader();
+
+  // TODO: Add a paywall
+
+  return (
+    <CSVReader onUploadAccepted={onUpload}>
+      {({ getRootProps }) => (
+        <Button
+          size="sm"
+          className="w-full lg:w-auto bg-dark-200 text-white"
+          {...getRootProps()}
+        >
+          <Upload className="size-4 mr-2" />
+          Import
+        </Button>
+      )}
+    </CSVReader>
+  );
+};
+
+export default UploadButton;
